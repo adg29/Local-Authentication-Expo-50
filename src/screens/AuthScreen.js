774854e83780b918d843300d6c786955435520a5
Alan Garcia <alandavidgarcia@gmail.com>
2024-02-18 14:31:46 -0500

    Update Auth and Vault screens
    Redesigned AuthScreen with a new brand image and title "InstantVerify" and switching the authenticated action description to emphasize fast identity verification. Improved layout with SafeAreaView for better spacing and responsive design by integrating Dimensions. Modified styles for broader device compatibility and aesthetic adjustments.
    
    Refactored PaymentScreen to include responsive image sizing and roundness. Changed the title to "Vault" and updated the placeholder image source to match new branding. Layout and style changes are minor but enhance the consistency and usability across various devices.

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,51 +1,69 @@
-import {View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
-const myImage = require('../../assets/1.png');
+import {
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Dimensions,
+} from "react-native";
+const myImage = require("../../assets/2.jpg");
+const { width, height } = Dimensions.get("window");
 
-export default function Auth({onAuthenticate}) {
-    return(
-        <View>
-            <Image source={myImage} style={styles.image}/>
-            <Text style={styles.title}>EXPENIO</Text>
-            <Text style={styles.description}>Going cashless has never been this easier with the world’s most leading expense manager.</Text>
-            <TouchableOpacity 
-                onPress={onAuthenticate}
-                style={styles.btn}>
-                <Text style={styles.text}>Login</Text>
-            </TouchableOpacity>
-        </View>
-    )
+export default function Auth({ onAuthenticate }) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image
+        source={myImage}
+        style={[styles.image, { width: width * 1, height: height * 0.5 }]} // Adjusted to be responsive
+      />
+      <Text style={styles.title}>InstantVerify</Text>
+      <Text style={styles.description}>
+        Secure your experience with lightning-fast identity verification, all in
+        one tap.
+      </Text>
+      <TouchableOpacity onPress={onAuthenticate} style={styles.btn}>
+        <Text style={styles.text}>Login</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    btn: {
-        width: 200,
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        backgroundColor: '#0893FC',
-        padding: 10,
-        borderRadius: 5,
-    },
-    image: {
-        width: 400,
-        height: 461,
-    },
-    text: {
-        color: '#fff',
-        fontSize: 20,
-        fontWeight: '500',
-    },
-    title: {
-        fontSize: 50,
-        fontWeight: '400',
-        marginVertical: 30,
-        textAlign: 'center',
-    },
-    description: {
-        fontSize: 18,
-        color: 'gray',
-        textAlign: 'center',
-        marginHorizontal: 20,
-        marginBottom: 50,
-    }
-})
+  container: {
+    alignItems: "center", // Added to center content on the screen
+    justifyContent: "center",
+    flex: 1, // Make sure the container takes full height
+  },
+  btn: {
+    width: "90%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#0893FC",
+    padding: 10,
+    margin: 10,
+    borderRadius: 15,
+    marginTop: 50,
+  },
+  image: {
+    // Removed fixed sizes
+    resizeMode: "cover", // Adjust the resizeMode to maintain aspect ratio
+  },
+  text: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  title: {
+    fontSize: 24, // Adjusted for better scaling
+    fontWeight: "400",
+    marginVertical: 30,
+    textAlign: "center",
+  },
+  description: {
+    fontSize: 16, // Adjusted for better scaling
+    color: "gray",
+    textAlign: "center",
+    marginHorizontal: 20,
+    marginBottom: 50,
+  },
+});
